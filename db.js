0735fef007f4e633d0cb7f0602e759e66b0a6d06
Matej Galic <matej.galic2013@gmail.com>
2024-10-26 23:06:11 +0200

    Changing code in db

--- db.js
+++ db.js
@@ -8,10 +8,10 @@ const password = process.env.PASSWORD;
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: `${user}`,         // Your PostgreSQL username
-    host: `${host}`,             // Usually 'localhost' or the Render host URL
-    database: `${database}`,     // The name of your database
-    password: `${password}`,     // Your PostgreSQL password
+    user: process.env.USER,         // Your PostgreSQL username
+    host: process.env.HOST,             // Usually 'localhost' or the Render host URL
+    database: process.env.DATABASE,     // The name of your database
+    password: process.env.PASSWORD,     // Your PostgreSQL password
     port: 5432,                    // Default PostgreSQL port
     ssl: {
         rejectUnauthorized: false,                // This option can be used in development but should be handled differently in production
